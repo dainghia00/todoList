408bdcf17cefecdae3f76ed2971471a56404993c
Dai Nghia <dainghia18112000@gmail.com>
2023-04-05 15:04:36 +0700

    add the getAge

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  getAge(): string {
+    return this.appService.getAge();
+  }
+  @Get('Hello')
   getHello(): string {
     return this.appService.getHello();
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getAge(): string {
+    return '123';
+  }
   getHello(): string {
     return 'Hello World 111!';
   }
