e217e3219b1e885020c514928b6bb075b316ca78
Dai Nghia <dainghia18112000@gmail.com>
2023-04-07 17:57:18 +0700

    get Data, get Data by ID, create Data, delete Data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (): Promise<any> => {
+        return {
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'postgres',
+          database: 'todo-lists',
+          synchronize: true,
+        };
+      },
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
